300b1fa86add7eab6ac676f6ac6cf21f931ccc22
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-07-03 09:31:58 +0200

    Create db.php

---
+++ db/db.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set("Europe/Paris");
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'syskeylmao');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'anexample');
+define('ERROR_MESSAGE', 'Oh fuck i can't connect to the database! :(');
+
+try {
+	$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+} catch( PDOException $Exception ) {
+	die(ERROR_MESSAGE);
+}
+?>
