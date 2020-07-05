634122d18dbde8c16a9a6c36fbe01944bac19c70
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2020-07-05 22:27:48 +0200

    Forgot the escape

--- db/db.php
+++ db/db.php
@@ -5,7 +5,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'syskeylmao');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'anexample');
-define('ERROR_MESSAGE', 'Oh fuck i can't connect to the database! :(');
+define('ERROR_MESSAGE', 'Oh fuck i can\'t connect to the database! :(');
 
 try {
 	$odb = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
